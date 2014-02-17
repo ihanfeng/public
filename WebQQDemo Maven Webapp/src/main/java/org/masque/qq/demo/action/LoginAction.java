1ca8969c8d322dbba51fc8d3dc6b40dcd462c83d
masques <masques@sina.com>
2014-02-17 16:48:42 +0800

    commit

--- WebQQDemo Maven Webapp/src/main/java/org/masque/qq/demo/action/LoginAction.java
+++ WebQQDemo Maven Webapp/src/main/java/org/masque/qq/demo/action/LoginAction.java
@@ -1,5 +1,11 @@
 package org.masque.qq.demo.action;
-
+/**
+ * 
+ * <p>Project: Masque's Base</p>
+ * <p>Description: </p>
+ * <p>Copyright (c) 2014 Masque.All Rights Reserved.</p>
+ * @author <a href="masque.java@gmail.com">Masque</a>
+ */
 public class LoginAction {
 
 }
---
+++ WebQQDemo Maven Webapp/src/main/java/org/masque/qq/demo/base/BaseAction.java
@@ -0,0 +1,5 @@
+package org.masque.qq.demo.base;
+
+public class BaseAction {
+
+}
